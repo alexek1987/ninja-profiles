367eb1155e89f88c8e99138106b7e0dadf8cd0ca
Alexander <alexander@Alex.local>
2021-03-16 21:39:05 +0700

    configure babel transform arrow functions in package.json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import "react-app-polyfill/ie11";
+ import "react-app-polyfill/stable";
 import React from "react";
-import 'react-app-polyfill/ie11'
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
